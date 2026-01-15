9f7c29df345f91828a27dc6fa014b9ec2b52110c
eriko13 <erikgerman917@gmail.com>
2026-01-15 09:07:32 -0700

    Update .gitignore to ensure node_modules is ignored and add a new route to fetch a contact by ID in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const { initDatabase } = require('./data/database');
+const { ObjectId } = require('mongodb');
 
 router.get('/', async (req, res) => {
     db = await initDatabase();
     const collection = db.collection('Contacts');
     const docs = await collection.find().toArray();
     res.json(docs);
+});
+
+router.get('/:id', async (req, res) => {
+    db = await initDatabase();
+    const collection = db.collection('Contacts');
+    const doc = await collection.findOne({ _id: new ObjectId(req.params.id) });
+    res.json(doc);
 });
 
 module.exports = router;
